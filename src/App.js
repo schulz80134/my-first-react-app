c5d0198a891e355e953708f8bafeda9158ea3159
Mike Schulz <mike_schulz@cable.comcast.com>
2020-06-30 13:19:06 -0600

    Initial version

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img
+        src="https://media.giphy.com/media/aosOPhpJHrJq8/giphy.gif"
+        className="App-logo"
+        alt="logo"
+      />
+        <p>Hello, my name is Mike Schulz</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

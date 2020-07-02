52874935b3c2c43d442a35c6a00eca78f6fa9928
Mike Schulz <mike_schulz@cable.comcast.com>
2020-07-02 11:33:46 -0600

    Updates

---
+++ precache-manifest.ff9464901bffa0b7ae26376a0b0d18bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d0a1009045c6a3d879ffc9724d171a",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "d0f45476bfede1ee71ca",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd897298eb18cc684266",
+    "url": "/my-first-react-app/static/js/2.0812576a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.0812576a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f45476bfede1ee71ca",
+    "url": "/my-first-react-app/static/js/main.df6d9146.chunk.js"
+  },
+  {
+    "revision": "60b7e66da55df6bcf495",
+    "url": "/my-first-react-app/static/js/runtime-main.f0a211c7.js"
+  }
+]);
